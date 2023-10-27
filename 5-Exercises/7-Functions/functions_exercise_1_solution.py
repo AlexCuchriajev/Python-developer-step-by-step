a5e0b7b5a85919e6a471bcfd0b7fdb7768815849
Aleksandr Cuchriajev <148259750+AlexCuchriajev@users.noreply.github.com>
2023-10-27 20:06:55 +0300

    add exercise #1 solution

---
+++ 5-Exercises/7-Functions/functions_exercise_1_solution.py
@@ -0,0 +1,14 @@
+## 8-Functions. Exercise #1 solution:
+# Using string multiplication:
+def say_hello():
+    print("HELLO\n"*5)
+
+# Another method
+def say_hello():
+    print("HELLO")
+    print("HELLO")
+    print("HELLO")
+    print("HELLO")
+    print("HELLO")
+
+say_hello()
